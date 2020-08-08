9a5b58da3fc56e76b2ad8ab76f902237f5b0cfb1
Priyanka Sachan <56231576+Priyanka-Sachan@users.noreply.github.com>
2020-08-08 13:22:26 +0530

    Tab bar changes to bottom bar

--- meeeal/lib/widgets/tab_bar_page.dart
+++ meeeal/lib/widgets/tab_bar_page.dart
@@ -8,43 +8,57 @@ class TabBarPage extends StatefulWidget {
 }
 
 class _TabBarPageState extends State<TabBarPage> {
+
+  int _selectedPageIndex=0;
+  List<Map<String,Object>> _pages=[
+    {'title':'Meeals','page':CategoriesPage()},
+    {'title':'Favourites','page':FavouritesPage()}
+  ];
+
+  void _selectPage(int index){
+    setState(() {
+      _selectedPageIndex=index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        backgroundColor: Colors.grey.shade500,
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: Colors.transparent,
-          title: Text(
-            'Meeeals',
-            textScaleFactor: 1.8,
-            style: TextStyle(letterSpacing: 1.5),
-          ),
-          leading: Container(
-            margin: EdgeInsets.symmetric(vertical: 0, horizontal: 8),
-            child: Icon(
-              Icons.fastfood,
-              size: 50,
-            ),
-          ),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Category',
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: 'Favourites',
-              )
-            ],
+
+    return Scaffold(
+      backgroundColor: Colors.grey.shade500,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          _pages[_selectedPageIndex]['title'],
+          textScaleFactor: 1.8,
+          style: TextStyle(letterSpacing: 1.5),
+        ),
+        leading: Container(
+          margin: EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+          child: Icon(
+            Icons.fastfood,
+            size: 50,
           ),
         ),
-        body: TabBarView(
-          children: <Widget>[CategoriesPage(), FavouritesPage()],
-        ),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        currentIndex: _selectedPageIndex,
+        unselectedItemColor:Colors.white,
+        selectedItemColor:Colors.tealAccent,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categories')
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              title: Text('Favorites')
+          )
+        ],
       ),
     );
   }
